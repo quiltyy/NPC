b66c52dcb46800f79fce277319586ac2efec27ae
quiltyy <achamberlain008@gmail.com>
2018-04-20 23:03:42 -0700

    Removed: diceroll mechanic Created: Mannerisms and Interactions Commented: Index Matching

--- NPC.py
+++ NPC.py
@@ -6,15 +6,6 @@
 #
 import random
 
-def diceroll(dice):
-d100 = random.randint(1,100)
-d20 = random.randint(1,20)
-d12 = random.randint(1,12)
-d10 = random.randint(0,9)
-d8 = random.randint(1,8)
-d6 = random.randint(1,6)
-d4 = random.randint(1,4)
-
 def NPCAppearance():
     NPCAppearanceList = ["Distinctive jewlery: earrings, necklace, circlet, bracelets", "Piercings",
     "Flamboyant or outlandish cloths", "Formal, clean clothes","Ragged, dirty clothes",
@@ -22,26 +13,56 @@ def NPCAppearance():
     "Tattoos", "Birthmark", "Unusual skin color", "Bald", "Braided beard or hair", "Unusual hair color",
     "Nervous eye twitch", "Distinctive nose", "Distinctive posture (crooked or rigid)", 
     "Exceptionally beautiful", "Exceptionally ugly"]
+    NPCAppearanceDesc = random.choice(NPCAppearanceList)
+    #NPCAppearanceIndex = NPCAppearanceList.index(NPCAppearanceDesc)
+    print(NPCAppearanceDesc)
 
 def NPCHighAbility():
     NPCHighAbilityList = ["Strength - powerful, brawny, strong as an ox", "Dexterity - lithe, agile, graceful",
     "Constitution - hardy, hale, healthy", "Intelligence-studious, learned, inquisitive", 
     "Wisdom - perceptive, spiritual, insightful", "Charisma - persuasive, forceful, born leader"]
     NPCHighAbilityDesc = random.choice(NPCHighAbilityList)
-    NPCHighAbilityIndex = NPCHighAbilityList.index(NPCHighAbilityDesc) 
+    #NPCHighAbilityIndex = NPCHighAbilityList.index(NPCHighAbilityDesc)
+    print(NPCHighAbilityDesc)
 
 def NPCLowAbility():
     NPCLowAbilityList = ["Strength - feeble, scrawny", "Dexterity - clumsy, fumbling", "Constitution - sickly, pale",
     "Intelligence - dim-witted, slow", "Wisdom - oblivious, absentminded", "Charisma - dull, boring"]
     NPCLowAbilityDesc = random.choice(NPCLowAbilityList)
-    NPCLowAbilityIndex = NPCLowAbilityList.index(NPCLowAbilityDesc)
+    #NPCLowAbilityIndex = NPCLowAbilityList.index(NPCLowAbilityDesc)
+    print(NPCLowAbilityDesc)
 
 def NPCTalent():
-    NPCTalenList = ["Plays a musical instrument", "Speaks several languages fluently", "Unbelievably lucky", "Perfect memory",
+    NPCTalentList = ["Plays a musical instrument", "Speaks several languages fluently", "Unbelievably lucky", "Perfect memory",
     "Great with animals", "Great with children", "Great at solving puzzles", "Great at one game", "Great at impersonations",
     "Draws beautifully", "Paints beautifully", "Sings beautifully", "Drinks everyone under the table", "Exper carpenter",
     "Expert cook", "Expert dart thrower and rock skipper", "Expert juggler", "Skilled actor and master of disguise", 
     "Skilled dancer", "Knows thieves' cant"]
-    NPCTalentDesc = random.choice(NPCTalent)
-    NPCTalentIndex = NPCTalenList.index(NPCTalentDesc)
+    NPCTalentDesc = random.choice(NPCTalentList)
+    #NPCTalentIndex = NPCTalenList.index(NPCTalentDesc)
+    print(NPCTalentDesc)
+
+def NPCMannerisms():
+    NPCMannerismsList = ["Prone to singing, whistling, or humming quietly", "Speaks in rhyme or some other peculiar way",
+    "Particularly low or high voice", "Slurs words lisps, or stutters", "Enunciates overly clearly", "Speaks loudly", "Whispers",
+    "Uses flowery speech or long words", "Uses colorful oaths and exclamations", "Makes constant jokes or puns", "Prone to predictions of doom",
+    "Fidgets", "Squints", "Stares into the distance", "Chews something", "Paces", "Taps fingers", "Bites fingernails", "Twirls hair or tugs beard"]
+    NPCMannerismsDesc = random.choice(NPCMannerismsList)
+    #NPCMannerismsIndex = NPCMannerismsList.index(NPCMannerismsDesc)
+    print(NPCMannerismsDesc)
+
+def NPCInteraction():
+    NPCInteractionList = ["Argumentative", "Arrogant", "Blustering", "Rude", "Curios", "Friendly", "Honest", "Hot tempered", "Irritable", "Ponderous",
+    "Quiet", "Suspicious"]
+    NPCInteractionDesc = random.choice(NPCInteractionList)
+    #NPCInteractionIndex = NPCInteratcionList.index(NPCInteractionDesc)
+    print(NPCInteractionDesc)
+
+NPCAppearance()
+NPCMannerisms()
+NPCInteraction
+NPCHighAbility()
+NPCLowAbility()
+NPCTalent()
+
 
