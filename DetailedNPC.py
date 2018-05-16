1029616d98f294a7065a35816242857dcada4b23
Andrew Chamberlain <Quilty@Andrews-MacBook-Pro-2.local>
2018-05-16 12:25:51 -0700

    Updated Formatting

--- DetailedNPC.py
+++ DetailedNPC.py
@@ -3,141 +3,141 @@
 # Python Ver 3.6.5
 # Author:  Andrew Chamberlain
 # Date:    4/20/2018
-#Last Updated: 4/21/2018
+#Last Updated: 5/15/2018
 ######################################
 #
 import random
 
-def NPCAppearance():
-    NPCAppearanceList = ["Distinctive jewlery: earrings, necklace, circlet, bracelets", "Piercings",
+def npc_appearance():
+    npc_appearance_list = ["Distinctive jewlery: earrings, necklace, circlet, bracelets", "Piercings",
     "Flamboyant or outlandish cloths", "Formal, clean clothes","Ragged, dirty clothes",
     "Pronounced scar", "Missing teeth", "Missing fingers", "Unusal eye colo (or two different colors)",
     "Tattoos", "Birthmark", "Unusual skin color", "Bald", "Braided beard or hair", "Unusual hair color",
     "Nervous eye twitch", "Distinctive nose", "Distinctive posture (crooked or rigid)", 
     "Exceptionally beautiful", "Exceptionally ugly"]
-    NPCAppearanceDesc = random.choice(NPCAppearanceList)
-    #NPCAppearanceIndex = NPCAppearanceList.index(NPCAppearanceDesc)
-    print("Appearance:",NPCAppearanceDesc)
+    npc_appearance_desc = random.choice(npc_appearance_list)
+    #NPCAppearanceIndex = npc_appearance_list.index(npc_appearance_desc)
+    print("Appearance:",npc_appearance_desc)
 
-def NPCHighAbility():
-    NPCHighAbilityList = ["Strength - powerful, brawny, strong as an ox", "Dexterity - lithe, agile, graceful",
+def npc_high_ability():
+    npc_high_ability_list = ["Strength - powerful, brawny, strong as an ox", "Dexterity - lithe, agile, graceful",
     "Constitution - hardy, hale, healthy", "Intelligence - studious, learned, inquisitive", 
     "Wisdom - perceptive, spiritual, insightful", "Charisma - persuasive, forceful, born leader"]
-    NPCHighAbilityDesc = random.choice(NPCHighAbilityList)
-    #NPCHighAbilityIndex = NPCHighAbilityList.index(NPCHighAbilityDesc)
-    print("High Ability:",NPCHighAbilityDesc)
+    npc_high_ability_desc = random.choice(npc_high_ability_list)
+    #NPCHighAbilityIndex = npc_high_ability_list.index(npc_high_ability_desc)
+    print("High Ability:",npc_high_ability_desc)
 
-def NPCLowAbility():
-    NPCLowAbilityList = ["Strength - feeble, scrawny", "Dexterity - clumsy, fumbling", "Constitution - sickly, pale",
+def npc_low_ability():
+    npc_low_ability_list = ["Strength - feeble, scrawny", "Dexterity - clumsy, fumbling", "Constitution - sickly, pale",
     "Intelligence - dim-witted, slow", "Wisdom - oblivious, absentminded", "Charisma - dull, boring"]
-    NPCLowAbilityDesc = random.choice(NPCLowAbilityList)
-    #NPCLowAbilityIndex = NPCLowAbilityList.index(NPCLowAbilityDesc)
-    print("Low Ability:",NPCLowAbilityDesc)
+    npc_low_ability_desc = random.choice(npc_low_ability_list)
+    #NPCLowAbilityIndex = npc_low_ability_list.index(npc_low_ability_desc)
+    print("Low Ability:",npc_low_ability_desc)
 
-def NPCTalent():
-    NPCTalentList = ["Plays a musical instrument", "Speaks several languages fluently", "Unbelievably lucky", "Perfect memory",
+def npc_talent():
+    npc_talent_list = ["Plays a musical instrument", "Speaks several languages fluently", "Unbelievably lucky", "Perfect memory",
     "Great with animals", "Great with children", "Great at solving puzzles", "Great at one game", "Great at impersonations",
     "Draws beautifully", "Paints beautifully", "Sings beautifully", "Drinks everyone under the table", "Expert carpenter",
     "Expert cook", "Expert dart thrower and rock skipper", "Expert juggler", "Skilled actor and master of disguise", 
     "Skilled dancer", "Knows thieves' cant"]
-    NPCTalentDesc = random.choice(NPCTalentList)
-    #NPCTalentIndex = NPCTalenList.index(NPCTalentDesc)
-    print("Talent:",NPCTalentDesc)
+    npc_talent_desc = random.choice(npc_talent_list)
+    #NPCTalentIndex = NPCTalenList.index(npc_talent_desc)
+    print("Talent:",npc_talent_desc)
 
-def NPCMannerisms():
-    NPCMannerismsList = ["Prone to singing, whistling, or humming quietly", "Speaks in rhyme or some other peculiar way",
+def npc_mannerisms():
+    npc_mannerisms_list = ["Prone to singing, whistling, or humming quietly", "Speaks in rhyme or some other peculiar way",
     "Particularly low or high voice", "Slurs words lisps, or stutters", "Enunciates overly clearly", "Speaks loudly", "Whispers",
     "Uses flowery speech or long words", "Uses colorful oaths and exclamations", "Makes constant jokes or puns", "Prone to predictions of doom",
     "Fidgets", "Squints", "Stares into the distance", "Chews something", "Paces", "Taps fingers", "Bites fingernails", "Twirls hair or tugs beard"]
-    NPCMannerismsDesc = random.choice(NPCMannerismsList)
-    #NPCMannerismsIndex = NPCMannerismsList.index(NPCMannerismsDesc)
-    print("Mannerisms:",NPCMannerismsDesc)
+    npc_mannerisms_desc = random.choice(npc_mannerisms_list)
+    #NPCMannerismsIndex = npc_mannerisms_list.index(npc_mannerisms_desc)
+    print("Mannerisms:",npc_mannerisms_desc)
 
-def NPCInteraction():
-    NPCInteractionList = ["Argumentative", "Arrogant", "Blustering", "Rude", "Curios", "Friendly", "Honest", "Hot tempered", "Irritable", "Ponderous",
+def npc_interaction():
+    npc_interaction_list = ["Argumentative", "Arrogant", "Blustering", "Rude", "Curios", "Friendly", "Honest", "Hot tempered", "Irritable", "Ponderous",
     "Quiet", "Suspicious"]
-    NPCInteractionDesc = random.choice(NPCInteractionList)
-    #NPCInteractionIndex = NPCInteratcionList.index(NPCInteractionDesc)
-    print("Interactions:",NPCInteractionDesc)
+    npc_interaction_desc = random.choice(npc_interaction_list)
+    #NPCInteractionIndex = NPCInteratcionList.index(npc_interaction_desc)
+    print("Interactions:",npc_interaction_desc)
 
-def NPCIdeals():
-    NPCIdealGood = ["Beauty", "Charity", "Greater good", "Life", "Respect"]
-    NPCIdealLawful = ["Community", "Fairness", "Honor", "Logic", "Responsibility", "Tradition"]
-    NPCIdealNeutral = ["Balance", "Knowledge", "Live and let live", "Moderation", "Neutrality", "People"]
-    NPCIdealEvil = ["Domination", "Greed", "Might", "Pain", "Retribution", "Slaughter"]
-    NPCIdealChaotic = ["Change", "Creativity", "Freedom", "Independence", "No limits", "Whimsy"]
-    NPCIdealOther = ["Aspiration", "Discovery", "Glory", "Nation", "Redemption", "Self-knowledge"]
-    NPCIdealRandom = ["NPCIdealGood", "NPCIdealLawful", "NPCIdealNeutral", "NPCIdealEvil", 
-    "NPCIdealChaotic", "NPCIdealOther"]
+def npc_ideals():
+    npc_ideal_good = ["Beauty", "Charity", "Greater good", "Life", "Respect"]
+    npc_ideal_lawful = ["Community", "Fairness", "Honor", "Logic", "Responsibility", "Tradition"]
+    npc_ideal_neutral = ["Balance", "Knowledge", "Live and let live", "Moderation", "Neutrality", "People"]
+    npc_ideal_evil = ["Domination", "Greed", "Might", "Pain", "Retribution", "Slaughter"]
+    npc_ideal_chaotic = ["Change", "Creativity", "Freedom", "Independence", "No limits", "Whimsy"]
+    npc_ideal_other = ["Aspiration", "Discovery", "Glory", "Nation", "Redemption", "Self-knowledge"]
+    npc_ideal_random = ["npc_ideal_good", "npc_ideal_lawful", "npc_ideal_neutral", "npc_ideal_evil", 
+    "npc_ideal_chaotic", "npc_ideal_other"]
     #####IdealOptions = ["Good", "Lawful", "Neutral", "Evil", "Chaotic", "Other", "Random"]####
-    IdealChoice = input("Select an ideal: ")
-    if IdealChoice == 'Good':
-        NPCIdealsDesc = random.choice(NPCIdealGood)
-        print("Good Ideal:", NPCIdealsDesc)
-    elif IdealChoice == 'Lawful':
-        NPCIdealsDesc = random.choice(NPCIdealLawful)
-        print("Lawful Ideal:", NPCIdealsDesc)
-    elif IdealChoice == 'Neutral':
-        NPCIdealsDesc = random.choice(NPCIdealNeutral)
-        print("Neutral Ideal:", NPCIdealsDesc)
-    elif IdealChoice == 'Evil':
-        NPCIdealsDesc = random.choice(NPCIdealEvil)
-        print("Evil Ideal:", NPCIdealsDesc)
-    elif IdealChoice == 'Chaotic':
-        NPCIdealsDesc = random.choice(NPCIdealChaotic)
-        print("Chaotic Ideal:", NPCIdealsDesc)
-    elif IdealChoice == 'Other':
-        NPCIdealsDesc = random.choice(NPCIdealOther)
-        print("Other Ideal:", NPCIdealsDesc)
-    elif IdealChoice == 'Random':
-        NPCIdealRandomSelection = random.choice(NPCIdealRandom)
-        if NPCIdealRandomSelection == "NPCIdealGood":
-            NPCIdealsDesc = random.choice(NPCIdealGood)
-            print("Good Ideal:", NPCIdealsDesc)
-        elif NPCIdealRandomSelection == "NPCIdealLawful":
-            NPCIdealsDesc = random.choice(NPCIdealLawful)
-            print("Lawful Ideal:", NPCIdealsDesc)
-        elif NPCIdealRandomSelection == "NPCIdealNeutral":
-            NPCIdealsDesc = random.choice(NPCIdealNeutral)
-            print("Neutral Ideal:", NPCIdealsDesc)
-        elif NPCIdealRandomSelection == "NPCIdealEvil":
-            NPCIdealsDesc = random.choice(NPCIdealEvil)
-            print("Evil Ideal:", NPCIdealsDesc)
-        elif NPCIdealRandomSelection == "NPCIdealChaotic":
-            NPCIdealsDesc = random.choice(NPCIdealChaotic)
-            print("Chaotic Ideal:", NPCIdealsDesc)
-        elif NPCIdealRandomSelection == "NPCIdealOtherc":
-            NPCIdealsDesc = random.choice(NPCIdealOther)
-            print("Other Ideal:", NPCIdealsDesc)
+    ideal_choice = input("Select an ideal: ")
+    if ideal_choice.lower() == 'good':
+        npc_ideal_desc = random.choice(npc_ideal_good)
+        print("Good Ideal:", npc_ideal_desc)
+    elif ideal_choice.lower() == 'lawful':
+        npc_ideal_desc = random.choice(npc_ideal_lawful)
+        print("Lawful Ideal:", npc_ideal_desc)
+    elif ideal_choice.lower() == 'neutral':
+        npc_ideal_desc = random.choice(npc_ideal_neutral)
+        print("Neutral Ideal:", npc_ideal_desc)
+    elif ideal_choice.lower() == 'evil':
+        npc_ideal_desc = random.choice(npc_ideal_evil)
+        print("Evil Ideal:", npc_ideal_desc)
+    elif ideal_choice.lower() == 'chaotic':
+        npc_ideal_desc = random.choice(npc_ideal_chaotic)
+        print("Chaotic Ideal:", npc_ideal_desc)
+    elif ideal_choice.lower() == 'other':
+        npc_ideal_desc = random.choice(npc_ideal_other)
+        print("Other Ideal:", npc_ideal_desc)
+    elif ideal_choice.lower() == 'random':
+        npc_ideal_random_selection = random.choice(npc_ideal_random)
+        if npc_ideal_random_selection == "npc_ideal_good":
+            npc_ideal_desc = random.choice(npc_ideal_good)
+            print("Good Ideal:", npc_ideal_desc)
+        elif npc_ideal_random_selection == "npc_ideal_lawful":
+            npc_ideal_desc = random.choice(npc_ideal_lawful)
+            print("Lawful Ideal:", npc_ideal_desc)
+        elif npc_ideal_random_selection == "npc_ideal_neutral":
+            npc_ideal_desc = random.choice(npc_ideal_neutral)
+            print("Neutral Ideal:", npc_ideal_desc)
+        elif npc_ideal_random_selection == "npc_ideal_evil":
+            npc_ideal_desc = random.choice(npc_ideal_evil)
+            print("Evil Ideal:", npc_ideal_desc)
+        elif npc_ideal_random_selection == "npc_ideal_chaotic":
+            npc_ideal_desc = random.choice(npc_ideal_chaotic)
+            print("Chaotic Ideal:", npc_ideal_desc)
+        elif npc_ideal_random_selection == "NPCIdealOtherc":
+            npc_ideal_desc = random.choice(npc_ideal_other)
+            print("Other Ideal:", npc_ideal_desc)
         else:
             print("There was an error in your ideals request")
     else:
         print("You did not select an available ideal")
 
-def NPCBond():
-    NPCBondList = ["Dedicated to fulfilling a personal life goal", "Protective of close family members",
+def npc_bond():
+    npc_bond_list = ["Dedicated to fulfilling a personal life goal", "Protective of close family members",
     "Protective of colleagues or compatriots", "Loyal to a benefactor, patron, or employer",
     "Captivated by a romantic interest", "Drawn to a special place", "Protective of a sentimental keepsake",
     "Protective of a valuable possesssion", "Out for revenge"]
-    NPCBondDesc = random.choice(NPCBondList)
-    #NPCInteractionIndex = NPCInteratcionList.index(NPCInteractionDesc)
-    print("Bond:",NPCBondDesc)
+    npc_bond_desc = random.choice(npc_bond_list)
+    #NPCInteractionIndex = NPCInteratcionList.index(npc_interaction_desc)
+    print("Bond:",npc_bond_desc)
 
-def NPCFlaw():
-    NPCFlawList = ["Forbidden love or susceptibility to romance", "Enjoys decadent pleasures", "Arrogance",
+def npc_flaw():
+    npc_flaw_list = ["Forbidden love or susceptibility to romance", "Enjoys decadent pleasures", "Arrogance",
     "Envies another creature's possesssions or station", "Overpowering greed", "Prone to rage",
     "Has a powerful enemy", "Specific phobia", "Shameful or scandalous history", "Secret crime or misdeed",
     "Possession of forbidden lore", "Foolhardy bravery"]
-    NPCFlawDesc = random.choice(NPCFlawList)
-    #NPCInteractionIndex = NPCInteratcionList.index(NPCInteractionDesc)
-    print("Flaw:",NPCFlawDesc)
+    npc_flaw_desc = random.choice(npc_flaw_list)
+    #NPCInteractionIndex = NPCInteratcionList.index(npc_interaction_desc)
+    print("Flaw:",npc_flaw_desc)
 
-NPCIdeals()
-NPCFlaw()
-NPCBond()
-NPCAppearance()
-NPCMannerisms()
-NPCInteraction
-NPCHighAbility()
-NPCLowAbility()
-NPCTalent()
+npc_ideals()
+npc_flaw()
+npc_bond()
+npc_appearance()
+npc_mannerisms()
+npc_interaction
+npc_high_ability()
+npc_low_ability()
+npc_talent()
--- Villains.py
+++ Villains.py
@@ -5,136 +5,136 @@
 # Python Ver 3.6.5
 # Author:  Andrew Chamberlain
 # Date:    4/20/2018
-#Last Updated: 4/21/2018
+#Last Updated: 5/15/2018
 ######################################
 #
 
 import random
 
-def VScheme():
-    SchemeImmortality = ["Acquire a legendary item to prolong life", "Ascend to godhood",
+def vscheme():
+    scheme_immortality = ["Acquire a legendary item to prolong life", "Ascend to godhood",
     "Become undead or obtain a younger body", "Steal a planar creature's essence"]
-    SchemeInfluence = ["Seize a position of power or title", "Win a contest or tournament",
+    scheme_influence = ["Seize a position of power or title", "Win a contest or tournament",
     "Win favor with a powerful individual", "Place a pawn in a position of power"]
-    SchemeMagic = ["Obation an ancient artifact", "Build a construct or magical device",
+    scheme_magic = ["Obtain an ancient artifact", "Build a construct or magical device",
     "Carry out a deity's wishes", "Offer sacrifices to a deity", "Contact a lost deity or power",
     "Open a gate to another world"]
-    SchemeMayhem = ["Fullfill an apocalyptic prophecy", "Enact the vengeful will have a god or patron",
+    scheme_mayhem = ["Fullfill an apocalyptic prophecy", "Enact the vengeful will have a god or patron",
     "Spread a vile contagion", "Overthrow a government", "Trigger a natural disaster", 
     "Utterly destroy a bloodlone or clan"]
-    SchemePassion = ["Prolong the life of a loved one", "Prove  worthy of another person's love",
+    scheme_passion = ["Prolong the life of a loved one", "Prove  worthy of another person's love",
     "Raise or restore a dead loved one", "Destroy rivals for another person's affection"]
-    SchemePower = ["Conquer a region or incite a rebellion", "Seize control of an army",
+    scheme_power = ["Conquer a region or incite a rebellion", "Seize control of an army",
     "Become the power behind the throne", "gain the favor of a ruler"]
-    SchemeRevenge = ["Avenge a past humiliation or insult", "Avenge a past imprisonment or injury",
+    scheme_revenge = ["Avenge a past humiliation or insult", "Avenge a past imprisonment or injury",
     "Avenge the death of a loved one", "Gain the favor of a ruler"]
-    SchemeWealth = ["Control natural resources or trade", "Marry into wealth", "Plunder ancient ruins",
+    scheme_wealth = ["Control natural resources or trade", "Marry into wealth", "Plunder ancient ruins",
     "Retrieve stolen property and punish the theif"]
-    ObjectiveChoice = input("Select an objective: ")
-    if ObjectiveChoice == 'Immortality':
-        SchemeDesc = random.choice(SchemeImmortality)
+    objective_choice = input("Select an objective: ")
+    if objective_choice.lower() == 'immortality':
+        SchemeDesc = random.choice(scheme_immortality)
         print("Objective: Immortality")
         print("Villain Scheme:", SchemeDesc)
-    if ObjectiveChoice == 'Influence':
-        SchemeDesc = random.choice(SchemeInfluence)
+    elif objective_choice.lower() == 'influence':
+        SchemeDesc = random.choice(scheme_influence)
         print("Objective: Influence")
         print("Villain Scheme:", SchemeDesc)
-    if ObjectiveChoice == 'Magic':
-        SchemeDesc = random.choice(SchemeMagic)
+    elif objective_choice.lower() == 'magic':
+        SchemeDesc = random.choice(scheme_magic)
         print("Objective: Magic")
         print("Villain Scheme:", SchemeDesc)
-    if ObjectiveChoice == 'Mayhem':
-        SchemeDesc = random.choice(SchemeMayhem)
+    elif objective_choice.lower() == 'mayhem':
+        SchemeDesc = random.choice(scheme_mayhem)
         print("Objective: Mayhem")
         print("Villain Scheme:", SchemeDesc)
-    if ObjectiveChoice == 'Passion':
-        SchemeDesc = random.choice(SchemePassion)
+    elif objective_choice == 'passion':
+        SchemeDesc = random.choice(scheme_passion)
         print("Objective: Passion")
         print("Villain Scheme:", SchemeDesc)
-    if ObjectiveChoice == 'Magic':
-        SchemeDesc = random.choice(SchemePower)
+    elif objective_choice.lower() == 'power':
+        SchemeDesc = random.choice(scheme_power)
         print("Objective: Power")
         print("Villain Scheme:", SchemeDesc)
-    if ObjectiveChoice == 'Revenge':
-        SchemeDesc = random.choice(SchemeRevenge)
+    elif objective_choice.lower() == 'revenge':
+        SchemeDesc = random.choice(scheme_revenge)
         print("Objective: Revenge")
         print("Villain Scheme:", SchemeDesc)
-    if ObjectiveChoice == 'Wealth':
-        SchemeDesc = random.choice(SchemeWealth)
+    elif objective_choice.lower() == 'wealth':
+        SchemeDesc = random.choice(scheme_wealth)
         print("Objective: Wealth")
         print("Villain Scheme:", SchemeDesc)
     else:
         print("Please select another objective.")
 
-def VMethods():
-    Main = ['Agricultural Devastation', 'Assault or Beatings', 
+def vmethods():
+    main = ['Agricultural Devastation', 'Assault or Beatings', 
     'Bounty Hunting or Assassination', 'Captivity or Coercion', 'Confidence Scams',
     'Defamation', 'Dueling', 'Execution', 'Impersonation or Disguise', 'Lying or Perjury',
     'Magical Mayhem', 'Murder', 'Neglect', 'Politics', 'Religion', 'Stalking',
     'Theft or Property Crime', 'Torture', 'Vice', 'Warfare']
-    Agriculture = ['Blight', 'Crop', 'Drought', 'Famine']
-    Captivity = ['Bribery', 'Enticement', 'Eviction', 'Imprisonment', 'Kidnapping',
+    agriculture = ['Blight', 'Crop', 'Drought', 'Famine']
+    captivity = ['Bribery', 'Enticement', 'Eviction', 'Imprisonment', 'Kidnapping',
     'Legal Intimidation', 'Press Gangs', 'Shackling', 'Threats or Harassment']
-    Scams = ['Breach of Contract', 'Cheating', 'Fast Talking', 'Fine Print',
+    scams = ['Breach of Contract', 'Cheating', 'Fast Talking', 'Fine Print',
     'Fraud or Swindling', 'Quackery or Tricks']
-    Defamation = ['Framing', 'Gossipsing or Slander', 'Humiliation', 'Libel or Insults']
-    Execution = ['Beheading', 'Burning at the Stake', 'Burying Alive', 'Crucification',
+    defamation = ['Framing', 'Gossipsing or Slander', 'Humiliation', 'Libel or Insults']
+    execution = ['Beheading', 'Burning at the Stake', 'Burying Alive', 'Crucification',
     'Drawing and Quartering', 'Hanging', 'Impalement', 'Sacrifice (living)']
-    Magical = ['Hauntings', 'Illusions', 'Infernal Bargains', 'Mind Control', 'Petrification',
+    magical = ['Hauntings', 'Illusions', 'Infernal Bargains', 'Mind Control', 'Petrification',
     'Raising or Animating the Dead', 'Summoning Monsters', 'Weather Control']
-    Murder = ['Assassination', 'Cannibalism', 'Dismemberment', 'Drowning', 'Electrocution',
+    murder = ['Assassination', 'Cannibalism', 'Dismemberment', 'Drowning', 'Electrocution',
     'Euthanasia (involuntary)', 'Disease', 'Proisoning', 'Stabbing', 'Strangulation or Suffocation']
-    Politics = ['Betrayal or Treason', 'Conspiracy', 'Espionage or Spying', 'Genocide',
+    politics = ['Betrayal or Treason', 'Conspiracy', 'Espionage or Spying', 'Genocide',
     'Oppression', 'Raising Taxes']
-    Religion = ['Curses', 'Desecration', 'False Gods', 'Heresy or Cults']
-    Theft = ['Arson', 'Blackmail or Extortion', 'Burglary', 'Counterfeiting', 'Highway Robbery',
+    religion = ['Curses', 'Desecration', 'False Gods', 'Heresy or Cults']
+    theft = ['Arson', 'Blackmail or Extortion', 'Burglary', 'Counterfeiting', 'Highway Robbery',
     'Looting', 'Mugging', 'Poaching', 'Seizing Property', 'Smuggling']
-    Torture = ['Acid', 'Blinding', 'Branding', 'Racking', 'Thumbscrews', 'Whipping']
-    Vice = ['Adultery', 'Drugs or Alcohol', 'Gambling', 'Seduction']
-    Warfare = ['Ambush', 'Invasion', 'Massacre', 'Mercenaries', 'Rebellion', 'Terrorism']
-    RandomMain = random.choice(Main)
-    if RandomMain == 'Argricultural Devastation':
-        XMethods = random.choice(Agriculture)
-        print ('Method:', XMethods)
-    if RandomMain == 'Captivity or Coericion':
-        XMethods = random.choice(Captivity)
-        print ('Method:', XMethods)
-    if RandomMain == 'Confidence Scams':
-        XMethods = random.choice(Scams)
-        print ('Method:', XMethods)
-    if RandomMain == 'Defamation':
-        XMethods = random.choice(Defamation)
-        print ('Method:', XMethods)
-    if RandomMain == 'Execution':
-        XMethods = random.choice(Execution)
-        print ('Method:', XMethods)
-    if RandomMain == 'Magical Mayhem':
-        XMethods = random.choice(Magical)
-        print ('Method:', XMethods)
-    if RandomMain == 'Murder':
-        XMethods = random.choice(Murder)
-        print ('Method:', XMethods)
-    if RandomMain == 'Politics':
-        XMethods = random.choice(Politics)
-        print ('Method:', XMethods)
-    if RandomMain == 'Religion':
-        XMethods = random.choice(Religion)
-        print ('Method:', XMethods)
-    if RandomMain == 'Theft or Property Crime':
-        XMethods = random.choice(Theft)
-        print ('Method:', XMethods)
-    if RandomMain == 'Torture':
-        XMethods = random.choice(Torture)
-        print ('Method:', XMethods)
-    if RandomMain == 'Vice':
-        XMethods = random.choice(Vice)
-        print ('Method:', XMethods)
-    if RandomMain == 'Warfare':
-        XMethods = random.choice(Warfare)
-        print ('Method:', XMethods)
+    torture = ['Acid', 'Blinding', 'Branding', 'Racking', 'Thumbscrews', 'Whipping']
+    vice = ['Adultery', 'Drugs or Alcohol', 'Gambling', 'Seduction']
+    warfare = ['Ambush', 'Invasion', 'Massacre', 'Mercenaries', 'Rebellion', 'Terrorism']
+    random_main = random.choice(main)
+    if random_main == 'Argricultural Devastation':
+        v_methods = random.choice(agriculture)
+        print ('Method:', v_methods)
+    elif random_main == 'Captivity or Coericion':
+        v_methods = random.choice(captivity)
+        print ('Method:', v_methods)
+    elif random_main == 'Confidence Scams':
+        v_methods = random.choice(scams)
+        print ('Method:', v_methods)
+    elif random_main == 'Defamation':
+        v_methods = random.choice(defamation)
+        print ('Method:', v_methods)
+    elif random_main == 'Execution':
+        v_methods = random.choice(execution)
+        print ('Method:', v_methods)
+    elif random_main == 'Magical Mayhem':
+        v_methods = random.choice(magical)
+        print ('Method:', v_methods)
+    elif random_main == 'Murder':
+        v_methods = random.choice(murder)
+        print ('Method:', v_methods)
+    elif random_main == 'Politics':
+        v_methods = random.choice(politics)
+        print ('Method:', v_methods)
+    elif random_main == 'Religion':
+        v_methods = random.choice(religion)
+        print ('Method:', v_methods)
+    elif random_main == 'Theft or Property Crime':
+        v_methods = random.choice(theft)
+        print ('Method:', v_methods)
+    elif random_main == 'Torture':
+        v_methods = random.choice(torture)
+        print ('Method:', v_methods)
+    elif random_main == 'Vice':
+        v_methods = random.choice(vice)
+        print ('Method:', v_methods)
+    elif random_main == 'Warfare':
+        v_methods = random.choice(warfare)
+        print ('Method:', v_methods)
     else:
         print ('Sorry')
 
 
-VScheme()
-VMethods()
+vscheme()
+vmethods()
